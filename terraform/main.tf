578043126322295a7e126d775bf336d0b31665d7
Dylan R. Johnston <dylan.johnston@familyzone.com>
2022-08-29 18:12:36 +0800

    Fix AWS trust relationship

--- terraform/main.tf
+++ terraform/main.tf
@@ -161,7 +161,7 @@ resource "aws_iam_role" "github" {
         Condition = {
           StringEquals = {
             "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com",
-            "token.actions.githubusercontent.com:sub" = "repo:DylanRJohnston/splitwise:ref:refs/heads/main"
+            "token.actions.githubusercontent.com:sub" = "repo:DylanRJohnston/splitwise:environment:production"
           }
         }
       }
